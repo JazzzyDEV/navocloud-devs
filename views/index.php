5555593be2cb115f00024b2c193a5335d4db3f2f
Lakshmi Bhaskar <16p3034@kennedyglobalschool.edu.in>
2024-12-01 16:07:03 +0530

    First Commit

--- views/index.php
+++ views/index.php
@@ -1,5 +1,7 @@
 <link href="https://cdn.jsdelivr.net/npm/daisyui@4.12.10/dist/full.min.css" rel="stylesheet" type="text/css" />
 <script src="https://cdn.tailwindcss.com"></script>
+<link href="./components/app.css" rel="stylesheet">
+<script defer src="./components/app.js" type="text/javascript"></script>
 <!DOCTYPE html>
 <html lang="en">
 
@@ -18,17 +20,30 @@ if (file_exists('./components/header.php')) {
 } else {
     echo "header.php not found!";
 }
-
 ?>
+<head>
+<style>
+body {
+  background-image: url('../assets/imgs/front-page/bg.png');
+}
+
+
+.review-box {
+  backdrop-filter: blur(10px);
+  position: relative;
+  z-index: 1;
+}
+</style>
+</head>
 
  <!-- ========== END HEADER ========== -->
     <!-- Hero -->
     <div class="transition-all max-w-[85rem] mx-auto px-4 sm:px-6 lg:px-8 mt-[2.5rem]">
       <!-- Grid -->
-      <div class="transition-all grid lg:grid-cols-7 lg:gap-x-8 xl:gap-x-12 lg:items-center">
+      <div class="transition-all grid lg:grid-cols-7 lg:gap-x-8 xl:gap-x-12 lg:items-center backgro">
         <div class="transition-all lg:col-span-3">
           <h1 class="transition-all text-3xl font-bold tracking-wide text-white font-calsans lg:text-5xl">
-            NavoCloud Empowering Your
+          <span class="text-zinc-300"><span id="idk">NavoCloud Empowering Your</span> <span class="text-sky-500" id="WhoAmI"></span>.</span>
             <span class="transition-all relative whitespace-nowrap text-blue-400">
               <svg aria-hidden="true" viewBox="0 0 418 42"
                 class="transition-all absolute top-2/3 left-0 h-[0.58em] w-full fill-blue-300/70"
@@ -37,7 +52,7 @@ if (file_exists('./components/header.php')) {
                   d="M203.371.916c-26.013-2.078-76.686 1.963-124.73 9.946L67.3 12.749C35.421 18.062 18.2 21.766 6.004 25.934 1.244 27.561.828 27.778.874 28.61c.07 1.214.828 1.121 9.595-1.176 9.072-2.377 17.15-3.92 39.246-7.496C123.565 7.986 157.869 4.492 195.942 5.046c7.461.108 19.25 1.696 19.17 2.582-.107 1.183-7.874 4.31-25.75 10.366-21.992 7.45-35.43 12.534-36.701 13.884-2.173 2.308-.202 4.407 4.442 4.734 2.654.187 3.263.157 15.593-.78 35.401-2.686 57.944-3.488 88.365-3.143 46.327.526 75.721 2.23 130.788 7.584 19.787 1.924 20.814 1.98 24.557 1.332l.066-.011c1.201-.203 1.53-1.825.399-2.335-2.911-1.31-4.893-1.604-22.048-3.261-57.509-5.556-87.871-7.36-132.059-7.842-23.239-.254-33.617-.116-50.627.674-11.629.54-42.371 2.494-46.696 2.967-2.359.259 8.133-3.625 26.504-9.81 23.239-7.825 27.934-10.149 28.304-14.005.417-4.348-3.529-6-16.878-7.066Z">
                 </path>
               </svg>
-              <span class="transition-all relative">Online Presence</span>
+              
             </span>
           </h1>
 
